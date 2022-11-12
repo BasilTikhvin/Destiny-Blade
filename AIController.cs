de8c038956b70be5898b29425df8230926c5c128
BasilTikhvin <116716957+BasilTikhvin@users.noreply.github.com>
2022-11-12 14:56:45 +0400

    Add files via upload

---
+++ AIController.cs
@@ -0,0 +1,221 @@
+using DestinyBlade;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class AIController : MonoBehaviour
+{
+    public enum AIType
+    {
+        Idle,
+        Patrol
+    }
+
+    [SerializeField] private AIType _behaviourType;
+    [SerializeField] private LayerMask _enemyLayerMask;
+    [SerializeField] private PatrolPoint[] _patrolRoute;
+    [SerializeField] private float _npcSightDistance;
+    [SerializeField] private AttackPoint _npcAttackPoint;
+    [SerializeField] private float _attackRate;
+    [SerializeField] private Sensor _groundSensor;
+
+    private Fighter _npc;
+    private Animator _npcAnimator;
+    private SpriteRenderer _npcSpriteRenderer;
+
+    private int _nextPatrolPoint;
+    private Vector2 _movePoint;
+
+    private RaycastHit2D _npcSight;
+    private Destructible _attackTarget;
+    private Vector2 _attackPointPosition;
+    private float _timeAfterAttack;
+
+    private bool _isAttacking;
+
+    private void Start()
+    {
+        _npc = transform.GetComponent<Fighter>();
+        _npc.EventOnDeath.AddListener(ActionOnDeath);
+
+        _npcAnimator = transform.GetComponentInChildren<Animator>();
+        _npcSpriteRenderer = transform.GetComponentInChildren<SpriteRenderer>();
+
+        if (_npcAttackPoint != null)
+        {
+        _attackPointPosition = _npcAttackPoint.transform.localPosition;
+        }
+    }
+
+    private void Update()
+    {
+        ActionIdle();
+
+        ActionUpdateMovePoint();
+
+        ActionFindAttackTarget();
+
+        ActionAttack();
+    }
+
+    private void ActionIdle()
+    {
+        if (_npcSpriteRenderer.flipX == true)
+        {
+            _npc.FaceDirection = 1;
+        }
+        else
+        {
+            _npc.FaceDirection = -1;
+        }
+
+        _npc.HorizontalDirection = 0;
+
+        _npcAnimator.SetBool("isRunning", false);
+
+        if (_npc.IsBlocking) return;
+
+        _npcAnimator.SetBool("isBlocking", false);
+    }
+
+    private void ActionUpdateMovePoint()
+    {
+        ActionGetAttackPointPosition();
+
+        if (_attackTarget != null)
+        {
+            _movePoint = _attackTarget.transform.position + (transform.right * _npc.FaceDirection);
+
+            ActionMove();
+
+            return;
+        }
+
+        if (_behaviourType == AIType.Idle) return;
+
+        bool isInsidePatrolPoint = Vector2.Distance(_patrolRoute[_nextPatrolPoint].transform.position, _npc.transform.position) < _patrolRoute[_nextPatrolPoint].Radius;
+
+        if (isInsidePatrolPoint == true)
+        {
+            _nextPatrolPoint++;
+
+            if (_nextPatrolPoint == _patrolRoute.Length)
+            {
+                _nextPatrolPoint = 0;
+            }
+            _movePoint = _patrolRoute[_nextPatrolPoint].transform.position;
+        }
+        else
+        {
+            _movePoint = _patrolRoute[_nextPatrolPoint].transform.position;
+        }
+
+        ActionMove();
+    }
+    private void ActionGetAttackPointPosition()
+    {
+        if (_npc.FaceDirection == -1)
+        {
+            _npcAttackPoint.transform.localPosition = new Vector2(_attackPointPosition.x, _attackPointPosition.y);
+        }
+        else
+        {
+            _npcAttackPoint.transform.localPosition = new Vector2(-_attackPointPosition.x, _attackPointPosition.y);
+        }
+    }
+
+    private void ActionFindAttackTarget()
+    {
+        _npcSight = Physics2D.Raycast(transform.position, transform.position + (transform.right * _npcSightDistance * _npc.FaceDirection), 5, _enemyLayerMask);
+
+        if (_npcSight)
+        {
+            _attackTarget = _npcSight.collider.transform.root.GetComponent<Destructible>();
+
+            if (_attackTarget != null)
+            {
+                _movePoint = _attackTarget.transform.position + (transform.right * _npc.FaceDirection);
+            }
+        }
+    }
+
+    private void ActionMove()
+    {
+        if (_isAttacking || _npc.IsBlocking) return;
+
+        if ((_movePoint.x - _npc.transform.position.x) > 0)
+        {
+            _npc.HorizontalDirection = 1;
+
+            _npc.FaceDirection = 1;
+
+            _npcSpriteRenderer.flipX = true;
+
+            _npcAnimator.SetBool("isRunning", true);
+        }
+
+        if ((_movePoint.x - _npc.transform.position.x) < 0)
+        {
+            _npc.HorizontalDirection = -1;
+
+            _npc.FaceDirection = -1;
+
+            _npcSpriteRenderer.flipX = false;
+
+            _npcAnimator.SetBool("isRunning", true);
+        }
+    }
+
+    private void ActionAttack()
+    {
+        if (_isAttacking)
+        {
+            _timeAfterAttack += Time.deltaTime;
+
+            if (_timeAfterAttack >= _attackRate)
+            {
+                _isAttacking = false;
+
+                _npcAttackPoint.Attack(_npc.MaxAttackCombo);
+            }
+            return;
+        }
+
+        if (_attackTarget == null) return;
+
+        if (_groundSensor.IsTriggered == true && Vector2.Distance(_npc.transform.position, _attackTarget.transform.position) < _npcAttackPoint.AttackRangeRadius * 2)
+        {
+            if (_npc.StaminaUsage() == false) return;
+
+            _timeAfterAttack = 0;
+
+            _npcAnimator.SetTrigger("attack");
+
+            _isAttacking = true;
+        }
+    }
+
+    private void ActionBlock()
+    {
+        _npc.IsBlocking = false;
+
+        if (_isAttacking) return;
+
+        if (Input.GetMouseButton(1) == true && _groundSensor.IsTriggered == true)
+        {
+            _npcAnimator.SetBool("isBlocking", true);
+
+            _npc.IsBlocking = true;
+        }
+    }
+
+    private void ActionOnDeath()
+    {
+        _npc.EventOnDeath.RemoveListener(ActionOnDeath);
+
+        _npcAnimator.SetTrigger("dead");
+
+        enabled = false;
+        _npc.enabled = false;
+    }
+}
---
+++ MovementController.cs
@@ -0,0 +1,216 @@
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+namespace DestinyBlade
+{
+    public class MovementController : MonoBehaviour
+    {
+        [SerializeField] private Fighter _player;
+        [SerializeField] private Animator _playerAnimator;
+        [SerializeField] private SpriteRenderer _playerSpriteRenderer;
+        [SerializeField] private Sensor _groundSensor;
+        [SerializeField] private AttackPoint _playerAttackPoint;
+
+        private Vector2 _attackPointPosition;
+
+        private bool _isAttacking;
+        private bool _isRolling;
+
+        private float _rollTime;
+        private int _currentAttack;
+        private float _timeAfterAttack;
+
+        private void Start()
+        {
+            _attackPointPosition = _playerAttackPoint.transform.localPosition;
+
+            //_target.EventOnDeath.AddListener(OnDeath);
+        }
+
+        private void Update()
+        {
+            UpdateMovement();
+        }
+
+        private void UpdateMovement()
+        {
+            Idle();
+
+            Run();
+
+            Jump();
+
+            Roll();
+
+            Attack();
+
+            Block();
+        }    
+
+        private void Idle()
+        {
+            _player.HorizontalDirection = 0;
+
+            _playerAnimator.SetBool("isRunning", false);
+
+            if (_player.IsBlocking) return;
+
+            _playerAnimator.SetBool("isBlocking", false);
+        }
+
+        private void Run()
+        {
+            GetAttackPointPosition();
+
+            if (_isAttacking || _isRolling || _player.IsBlocking) return;
+
+            if (Input.GetKey(KeyCode.D) == true)
+            {
+                _player.HorizontalDirection = 1;
+
+                _player.FaceDirection = 1;
+
+                _playerSpriteRenderer.flipX = false;
+
+                _playerAnimator.SetBool("isRunning", true);
+            }
+
+            if (Input.GetKey(KeyCode.A) == true)
+            {
+                _player.HorizontalDirection = -1;
+
+                _player.FaceDirection = -1;
+
+                _playerSpriteRenderer.flipX = true;
+
+                _playerAnimator.SetBool("isRunning", true);
+            }
+        }
+
+        private void GetAttackPointPosition()
+        {
+            if (_player.FaceDirection == 1)
+            {
+                _playerAttackPoint.transform.localPosition = new Vector2(_attackPointPosition.x, _attackPointPosition.y);
+            }
+            else
+            {
+                _playerAttackPoint.transform.localPosition = new Vector2(-_attackPointPosition.x, _attackPointPosition.y);
+            }
+        }
+
+        private void Jump()
+        {
+            if (_isAttacking || _isRolling) return;
+
+            if (Input.GetKeyDown(KeyCode.Space) == true && _groundSensor.IsTriggered == true)
+            {
+                if (_player.StaminaUsage() == false) return;
+
+                _player.VerticalDirection = 1;
+
+                _playerAnimator.SetTrigger("jump");
+            }
+
+            if (_groundSensor.IsTriggered == false)
+            {
+                _player.VerticalDirection = 0;
+            }
+        }
+
+        private void Roll()
+        {
+            if (_isAttacking) return;
+
+            if (_rollTime > 1.0f)
+            {
+                _player.RollDirection = 0;
+
+                _isRolling = false;
+            }
+            else
+            {
+                _rollTime += Time.deltaTime;
+
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift) == true && _groundSensor.IsTriggered == true)
+            {
+                if (_player.StaminaUsage() == false) return;
+
+                _rollTime = 0f;
+
+                if (_playerSpriteRenderer.flipX == false)
+                {
+                    _player.RollDirection = 1;
+                }
+                else
+                {
+                    _player.RollDirection = -1;
+                }
+
+                _isRolling = true;
+
+                _playerAnimator.SetTrigger("roll");
+            }
+        }
+
+        private void Attack()
+        {
+            if (_isRolling) return;
+
+            if (_timeAfterAttack > 1.0f)
+            {
+                _isAttacking = false;
+            }
+            else
+            {
+                _timeAfterAttack += Time.deltaTime;
+
+                if (_timeAfterAttack > 0.6f)
+                {
+                    _currentAttack = 0;
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0) == true && _timeAfterAttack > 0.4f && _groundSensor.IsTriggered == true)
+            {
+                if (_player.StaminaUsage() == false) return;
+
+                _timeAfterAttack = 0;
+
+                _currentAttack++;
+
+                if (_currentAttack > _player.MaxAttackCombo)
+                {
+                    _currentAttack = 0;
+                }
+
+                if (_currentAttack >= 1)
+                {
+                    _playerAttackPoint.Attack(_currentAttack);
+
+                    _playerAnimator.SetTrigger("attack" + _currentAttack);
+
+                    _isAttacking = true;
+                }
+            }
+        }
+
+        private void Block()
+        {
+            _player.IsBlocking = false;
+
+            if (_isAttacking || _isRolling) return;
+
+            if (Input.GetMouseButton(1) == true && _groundSensor.IsTriggered == true)
+            {
+                _playerAnimator.SetBool("isBlocking",true);
+
+                _player.IsBlocking = true;
+            }
+        }
+    }
+}
